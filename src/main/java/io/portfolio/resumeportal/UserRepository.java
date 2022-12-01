299b4ed79868673ea774a55c6d275574f8e56f02
Prabhastha <prabhastha0@gmail.com>
2022-12-02 01:51:11 +0900

    Adding repository JPA infastructure

---
+++ src/main/java/io/portfolio/resumeportal/UserRepository.java
@@ -0,0 +1,10 @@
+package io.portfolio.resumeportal;
+
+import io.portfolio.resumeportal.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
